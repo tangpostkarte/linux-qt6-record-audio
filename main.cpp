2e9766989c10dd5187cd47d58f0b9785c0fc6d57
Your Name <you@example.com>
2023-12-27 21:20:56 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QDebug>
+
+extern "C" {
+    #include <libavutil/avutil.h>
+}
+
+int main(int argc, char *argv[])
+{
+
+    qDebug() << av_version_info();
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
